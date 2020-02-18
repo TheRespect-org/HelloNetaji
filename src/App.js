573b424ed3693f39e846e11be4829697f4a0a0a9
Zameer Ansari <zameer.ansari@instarem.com>
2020-02-18 21:13:48 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="jumbotron text-center">
+        <h1>Hello Netaji! NPR hi NRC hai!</h1>
+        <p>Resize this responsive page to see the effect!</p>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div class="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div class="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
