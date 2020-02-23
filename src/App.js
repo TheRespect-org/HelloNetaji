f6070b2d90935dd524fbf3bfc560cac23c6ebecf
Zameer Ansari <zameer.ansari@instarem.com>
2020-02-24 01:59:45 +0530

    updated social handles

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://www.instagram.com/hellonetaji/"><i className="fab fa-instagram"></i> hellonetaji </a>
+        <a href="https://www.instagram.com/hellonetaji/"><i className="fab fa-instagram"></i> HelloNetaji </a>
         <a href="https://facebook.com/HelloNetaji-102682481335358/"><i className="fab fa-facebook"></i> HelloNetaji </a>
-        <a href="https://twitter.com/hello_netaji"><i className="fab fa-twitter"></i> hello_netaji </a>
+        <a href="https://twitter.com/netajihello"><i className="fab fa-twitter"></i> NetajiHello </a>
         <label className="float-right" >v1.0.2</label>
       </div>
       <div className="jumbotron text-center">
