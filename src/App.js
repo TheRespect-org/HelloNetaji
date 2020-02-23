f3408d02bd8853a67d5ccdecb691539e29e5f86c
Zameer Ansari <zameer.ansari@instarem.com>
2020-02-24 04:59:18 +0530

    SEO updates

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <label className="float-right" >v1.0.2</label>
       </div>
       <div className="jumbotron text-center">
-        <h1>Hello Netaji!</h1>
+        <img style={{"width" : "10%"}} src="logos/logo.jpeg" />
         <p><a href="https://en.wikipedia.org/wiki/National_Register_of_Citizens"> NPR</a> 2020 se NRC banega <a href="https://twitter.com/intent/tweet?url=http%3A%2F%2Fhellonetaji.therespect.org%2F&text=Dear%20Netaji!%20Please%20stop%20NPR%20in%20Maharashtra%20Assembly.%20NPR%20is%20the%20first%20step%20of%20NRC%20which%20will%20mark%20people%20as%20doubtful%20citizens!%20%23HelloNetaji%20%23noNPR%20%23noNRC%20Find%20your%20Neta%20at%20">#NoNPR</a></p>
 
         <Petition />
@@ -25,10 +25,6 @@ function App() {
           <div className="col-sm-1"></div>
 
           <div className="col-sm-10">
-            <p>
-              Our MLAs are law makers. We elect them to represent us and issues that concern us. The beauty of a federal democratic state is that you can and you must be connected to your elected representative.
-                        </p>
-
 
             <Netas />
 
